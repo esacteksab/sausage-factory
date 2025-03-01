0bf3317c2aee98298cbd6a1642e6b142a91d4db1
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2025-03-01 13:35:50 -0600

    science, lots of science

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
 resource "random_uuid" "uuid" {}
 
 resource "local_file" "uuid" {
+  count = 1000
   content  = random_uuid.uuid.id
-  filename = "${path.module}/uuid.out"
+  filename = "${path.module}/bsTF/${count.index}-uuid.out"
 }
 
 resource "random_pet" "pet" {}
 
 resource "local_file" "pet" {
+  count = 1000
   content  = random_pet.pet.id
-  filename = "${path.module}/pet.out"
+  filename = "${path.module}/bs/TF/${count.index}-pet.out"
 
 }
 
 resource "archive_file" "tf_pr" {
+  count = 1000
   type        = "tar.gz"
   source_file = "${path.module}/.tf-pr"
-  output_path = "${path.module}/tf-pr.tar.gz"
+  output_path = "${path.module}/bsTF/${count.index}-tf-pr.tar.gz"
 }
