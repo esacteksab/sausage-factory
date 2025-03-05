315168092a473609cf37670038d1c423b189b164
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2025-03-04 18:19:56 -0600

    chore: Don't need all of this

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "random_uuid" "uuid" {}
 
 resource "local_file" "uuid" {
-  count = 1000
+  count    = 10
   content  = random_uuid.uuid.id
   filename = "${path.module}/bsTF/${count.index}-uuid.out"
 }
@@ -9,15 +9,15 @@ resource "local_file" "uuid" {
 resource "random_pet" "pet" {}
 
 resource "local_file" "pet" {
-  count = 1000
+  count    = 10
   content  = random_pet.pet.id
   filename = "${path.module}/bs/TF/${count.index}-pet.out"
 
 }
 
-resource "archive_file" "tf_pr" {
-  count = 1000
-  type        = "tar.gz"
-  source_file = "${path.module}/.tf-pr"
-  output_path = "${path.module}/bsTF/${count.index}-tf-pr.tar.gz"
-}
+# resource "archive_file" "tf_pr" {
+#   count       = 10
+#   type        = "tar.gz"
+#   source_file = "${path.module}/.tf-pr"
+#   output_path = "${path.module}/bsTF/${count.index}-tf-pr.tar.gz"
+# }
