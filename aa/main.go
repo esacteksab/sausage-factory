0d0f709b7271897eea08583f4ed735b4b088d0d6
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2025-04-05 16:16:44 -0500

    chore: I sync'd, not sure it's working

--- aa/main.go
+++ aa/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 
 	kt "github.com/knadh/koanf/parsers/toml/v2"
 	"github.com/knadh/koanf/providers/file"
@@ -13,8 +16,6 @@ import (
 
 func main() {
 	GetConfig()
-	// This is a simple Go program that prints "Hello, World!" to the console.
-	// It serves as a basic example of how to structure a Go application.
 }
 
 func GetConfig() {
@@ -30,14 +31,14 @@ func GetConfig() {
 	// fmt.Printf("Tables in config file: %s", tables)
 	for _, table := range tables {
 		// The Table
-		fmt.Println(table)
+		// fmt.Println(table)
 		// Get Path for each table
 		path := k.String(table + ".path")
-		fmt.Printf(" Path: %s\n", path)
+		// fmt.Printf(" Path: %s\n", path)
 
 		// Get Files for each table
 		files := k.Strings(table + ".files")
-		fmt.Printf(" Files: %s\n", files)
+		// fmt.Printf(" Files: %s\n", files)
 
 		CopyFile(path, files)
 
@@ -48,22 +49,73 @@ func GetConfig() {
 func CopyFile(path string, files []string) {
 	// This function will copy files from the source path to the destination path.
 	for _, file := range files {
-		fmt.Printf("Copying file from %s: %s\n", path, file)
-	}
-	sourceFile, err := os.Open(path)
-	if err != nil {
-		log.Fatalf(err)
-	}
-	defer sourceFile.Close()
+		// fmt.Printf("Copying file from %s: %s\n", path, file)
+		filePath := path + "/" + file
+		// fmt.Println(filePath)
+		// Open the source file for reading
+		sourceFile, err := os.Open(filePath)
+		// fmt.Printf("Opening file: %s\n", filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer sourceFile.Close()
 
-	destinationFile, err := os.Create(path)
-	if err != nil {
-		log.Fatalf(err)
+		// We need to create the destination director[y|ies] if it doesn't exist
+		fpath := filepath.Dir(file)
+
+		// If it doesn't exist, create it
+		if !doesExist(fpath) {
+			if err := os.MkdirAll(fpath, os.ModePerm); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		// Presumably we are copying to the current directory, so this is `file`
+		destinationFile, err := os.Create(file)
+		// fmt.Printf("Creating file: %s\n", file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer destinationFile.Close()
+
+		// Check to see if the files are the same prior to trying to copy them.
+		same, err := sameFile(filePath, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if same {
+			fmt.Printf("File %s already exists, skipping copy\n", file)
+		} else if !same {
+			// Copy the contents of the source file to the destination file
+			_, err = io.Copy(destinationFile, sourceFile)
+			fmt.Println("Files are not the same, copying...")
+			fmt.Printf("Copying file from %s: %s\n", path, file)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
-	defer destinationFile.Close()
-	// Copy the contents of the source file to the destination file
-	_, err = io.Copy(destinationFile, sourceFile)
+}
+
+func doesExist(path string) bool {
+	// Check if the file or directory exists
+	_, err := os.Stat(path)
+	return !errors.Is(err, fs.ErrNotExist)
+}
+
+func sameFile(file1, file2 string) (bool, error) {
+	// Get info about both files
+	f1, err := os.Stat(file1)
 	if err != nil {
-		log.Fatalf(err)
+		return false, err
 	}
+
+	f2, err := os.Stat(file2)
+	if err != nil {
+		return false, err
+	}
+	// Check if the file names are the same
+
+	return os.SameFile(f1, f2), nil
 }
